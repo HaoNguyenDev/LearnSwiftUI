765505c2ab69eed851c5bd31b82f37b025640344
HaoNguyen <>
2025-05-27 17:34:10 +0700

    Add Shapes with custom style.

---
+++ LearnSwiftUI/LearnSwiftUI/BootCamp/ShapesBootCamp.swift
@@ -0,0 +1,27 @@
+//
+//  ShapesBootCamp.swift
+//  LearnSwiftUI
+//
+//  Created by Hao Nguyen on 27/5/25.
+//
+
+import SwiftUI
+
+struct ShapesBootCamp: View {
+    var body: some View {
+//        Circle()
+//        ContainerRelativeShape()
+//        Capsule(style: .circular)
+//        Rectangle()
+        RoundedRectangle(cornerRadius: 20.0)
+            .fill(Color.red)
+//            .stroke(Color.blue, lineWidth: 10)
+            .stroke(Color.blue, style: StrokeStyle(lineWidth: 3.0, lineCap: .round, dash: [10.0]))
+            .padding(20.0)
+            .frame(width: 300.0, height: 200.0)
+    }
+}
+
+#Preview {
+    ShapesBootCamp()
+}
