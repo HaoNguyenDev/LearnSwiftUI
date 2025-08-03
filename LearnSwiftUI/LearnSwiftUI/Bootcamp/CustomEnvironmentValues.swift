33aa9b236fbfdbdc48c1f58165851a8ab6ffe90d
HaoNguyen <>
2025-08-03 17:37:31 +0700

    Add xample code with custom environment key

---
+++ LearnSwiftUI/LearnSwiftUI/Bootcamp/CustomEnvironmentValues.swift
@@ -0,0 +1,63 @@
+//
+//  CustomEnvironmentValues.swift
+//  LearnSwiftUI
+//
+//  Created by Hao Nguyen on 3/8/25.
+//
+
+import Foundation
+import SwiftUI
+
+//Step 1: Define the environment key
+struct APIEndPoint: EnvironmentKey {
+    static let defaultValue: String = "https://api.example.com" /// The default value for the environment key.
+}
+
+struct DebugModeKey: EnvironmentKey {
+    static let defaultValue: Bool = false /// The default value for the environment key.
+}
+
+//Step 2: Extend EnvironmentValues
+extension EnvironmentValues {
+    var apiPoint: String {
+        get {
+            self[APIEndPoint.self]
+        }
+        set {
+            self[APIEndPoint.self] = newValue
+        }
+    }
+    
+    var isDebugMode: Bool {
+        get {
+            self[DebugModeKey.self]
+        }
+        set {
+            self[DebugModeKey.self] = newValue
+        }
+    }
+}
+
+//Step 3: Use them
+
+struct EnvironmentKeyContentView: View {
+    var body: some View {
+        UseEnvironmentView() // Inject environment to view
+            .environment(\.apiPoint, "https://api.example.com/debug")
+            .environment(\.isDebugMode, true)
+    }
+}
+
+struct UseEnvironmentView: View {
+    @Environment(\.apiPoint)    private var apiPoint
+    @Environment(\.isDebugMode) private var isDebugMode
+    
+    var body: some View {
+        Text("apiPoint: \(apiPoint)")
+        Text("isDebugMode: \(isDebugMode)")
+    }
+}
+
+#Preview {
+    EnvironmentKeyContentView()
+}
