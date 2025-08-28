dae8156efc8cbb1f82eac9208cc1a007db860cc3
HaoNguyen <>
2025-08-28 21:39:51 +0700

    Add action on row of list

--- LearnSwiftUI/LearnSwiftUI/Bootcamp/ListBootcamp.swift
+++ LearnSwiftUI/LearnSwiftUI/Bootcamp/ListBootcamp.swift
@@ -7,10 +7,11 @@
 
 import SwiftUI
 
-struct TaskModel: Identifiable {
+struct TaskModel: Identifiable, Equatable {
     var id: UUID = UUID()
     var name: String
     var isDone: Bool = false
+    var isPinned: Bool = false
 }
 
 @Observable class TaskViewModel: ObservableObject {
@@ -18,6 +19,12 @@ struct TaskModel: Identifiable {
     
     func addTask(name: String) {
         tasks.append(TaskModel(name: name))
+    }
+    
+    func removeTask(_ task: TaskModel) {
+        if let indexOfTask = tasks.firstIndex(of: task) {
+            tasks.remove(at: indexOfTask)
+        }
     }
     
     func removeTask(at offSet: IndexSet) {
@@ -28,39 +35,70 @@ struct TaskModel: Identifiable {
         tasks.move(fromOffsets: from, toOffset: to)
     }
     
-    func toggleTask(_ task: TaskModel) {
+    func toggleDoneTask(_ task: TaskModel) {
         if let indexOfTask = tasks.firstIndex(where: { $0.id == task.id }) {
             tasks[indexOfTask].isDone.toggle()
+        }
+    }
+    
+    func toggleIsPinnedTask(_ task: TaskModel) {
+        if let indexOfTask = tasks.firstIndex(where: { $0.id == task.id }) {
+            tasks[indexOfTask].isPinned.toggle()
         }
     }
     
 }
 struct ListBootcamp: View {
     @Environment(\.colorScheme) var colorScheme
-    @State var vm = TaskViewModel()
+    @StateObject private var vm = TaskViewModel()
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(vm.tasks) { task in
+                ForEach(vm.tasks.sorted { $0.isPinned && !$1.isPinned }) { task in
                     HStack {
                         Image(systemName: task.isDone ? "checkmark.circle" : "circle")
-                            .foregroundColor(Color.green)
+                            .foregroundColor(.green)
+                            .onTapGesture {
+                                vm.toggleDoneTask(task)
+                            }
                         
                         Text(task.name)
-                            .strikethrough(task.isDone, color: colorScheme == .dark ? .white : .black)
+                            .strikethrough(task.isDone,
+                                           color: colorScheme == .dark ? .white : .black)
+                        Spacer()
+                        Image(systemName: task.isPinned ? "pin" : "")
+                            .foregroundStyle(Color.yellow)
                     }
-                    .onTapGesture {
-                        vm.toggleTask(task)
+                    .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                        Button(role: .destructive) {
+                            vm.removeTask(task)
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                        }
+                        
+                        Button {
+                            vm.toggleIsPinnedTask(task)
+                        } label: {
+                            Label(task.isPinned ? "Unpin" : "Pin",
+                                  systemImage: task.isPinned ? "pin.slash" : "pin")
+                        }
+                    }
+                    .swipeActions(edge: .leading) {
+                        Button {
+                            vm.toggleDoneTask(task)
+                        } label: {
+                            Label(task.isDone ? "Uncomplete" : "Complete",
+                                  systemImage: task.isDone ? "arrow.uturn.backward.circle" : "checkmark.circle.fill")
+                        }
+                        .tint(task.isDone ? .gray : .green)
                     }
                 }
                 .onDelete(perform: deleteTask)
                 .onMove(perform: moveTask)
             }
             .navigationBarTitle("List Bootcamp")
-            .toolbar {
-                EditButton()
-            }
+            .toolbar { EditButton() }
         }
     }
     
