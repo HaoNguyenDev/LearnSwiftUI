b34f4c07717693702966ca52e5b9031a555f0a07
HaoNguyen <>
2025-09-04 12:56:12 +0700

    Add an custom app theme with cònig color and font

--- LearnSwiftUI/LearnSwiftUI/LearnSwiftUIApp.swift
+++ LearnSwiftUI/LearnSwiftUI/LearnSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LearnSwiftUIApp: App {
     @State private var sharedModel = SharedModel()
     @StateObject private var todoStore = TodoStore()
-    
+    @State private var themeVM = ThemeViewModel()
     var body: some Scene {
         WindowGroup {
 //            TextBootCamp()
@@ -43,7 +43,10 @@ struct LearnSwiftUIApp: App {
 //            FocusStateBootcamp()
 //            ListBootcamp()
 //            TextFieldBootcamp()
-            PickerBootcamp()
+//            PickerBootcamp()
+//            ThemedTextView()
+//                .environment(\.theme, themeVM.currentTheme)
+//                .environment(themeVM)
         }
     }
 }
