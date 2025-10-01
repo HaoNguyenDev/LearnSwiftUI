aac92b3e6c8bb34ad8cedf67b8e5aa2f7bff1dec
HaoNguyen <>
2025-10-01 23:21:23 +0700

    Add Combine framework and UIKit demo

---
+++ LearnSwiftUI/LearnSwiftUI/AppDemo/CombineAndUIKitDemo/Model/CBLogginResult.swift
@@ -0,0 +1,14 @@
+//
+//  CBLogginResult.swift
+//  LearnSwiftUI
+//
+//  Created by Hao Nguyen on 1/10/25.
+//
+
+import Foundation
+
+struct CBLogginResult {
+    let isSuccess: Bool
+    let error: Error?
+    let token: String?
+}
