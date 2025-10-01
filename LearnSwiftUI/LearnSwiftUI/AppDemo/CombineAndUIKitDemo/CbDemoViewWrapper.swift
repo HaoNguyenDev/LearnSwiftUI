984dace62ed8e1a32c8f0348bfea7c12b4ff645a
HaoNguyen <>
2025-10-01 23:51:48 +0700

    Add UIViewControllerRepresentable for SwiftUI

---
+++ LearnSwiftUI/LearnSwiftUI/AppDemo/CombineAndUIKitDemo/CbDemoViewWrapper.swift
@@ -0,0 +1,28 @@
+//
+//  CbDemoViewWrapper.swift
+//  LearnSwiftUI
+//
+//  Created by Hao Nguyen on 1/10/25.
+//
+
+import SwiftUI
+import UIKit
+
+// Replace MyViewController with the UIViewController name you want to display
+struct CbDemoViewWrapper: UIViewControllerRepresentable {
+    
+    // Required: Defines the type of UIViewController this struct represents
+    typealias UIViewControllerType = CbLoginViewController
+    
+    // Required: Method for creating and configuring an initial UIViewController
+    func makeUIViewController(context: Context) -> CbLoginViewController {
+        // Initialize your UIViewController
+        let myVC = CbLoginViewController()
+        // Add initial configuration if needed
+        return myVC
+    }
+    // Required: Method for updating a UIViewController when the SwiftUI state changes
+    func updateUIViewController(_ uiViewController: CbLoginViewController, context: Context) {
+        // Update the view controller if bindings or @State change
+    }
+}
