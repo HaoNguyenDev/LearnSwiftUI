22b86ebfed22831508cae6c633984ce127044929
HaoNguyen <>
2025-10-15 21:57:08 +0700

    Add demo combine work with SwiftUI Textfield

---
+++ LearnSwiftUI/LearnSwiftUI/CustomView/ViewModifiers/TextFieldModifiers.swift
@@ -0,0 +1,28 @@
+//
+//  TextFieldModifiers.swift
+//  LearnSwiftUI
+//
+//  Created by Hao Nguyen on 15/10/25.
+//
+
+import SwiftUI
+
+// MARK: - TextField View Modifier
+struct DefaultTextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(.leading)
+            .frame(height: 50)
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(10)
+            .padding(.horizontal, 16)
+    }
+}
+
+// MARK: - TextField Extension
+extension View {
+    func defaultTf() -> some View {
+        modifier(DefaultTextFieldModifier())
+    }
+}
+
