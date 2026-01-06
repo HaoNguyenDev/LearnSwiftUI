3d3610a710c90e0e138b1e15bef2df55baf5cad3
HaoNguyen <>
2026-01-06 12:42:53 +0700

    Refactor code with lesson for all bootcamp

---
+++ LearnSwiftUI/LearnSwiftUI/CustomView/CodePreviewContainer/Lesson.swift
@@ -0,0 +1,15 @@
+//
+//  Lesson.swift
+//  LearnSwiftUI
+//
+//  Created by Hao Nguyen on 6/1/26.
+//
+
+import SwiftUI
+
+struct Lesson: Identifiable {
+    let id = UUID()
+    let title: String
+    let code: String
+    let result: (() -> AnyView)?
+}
