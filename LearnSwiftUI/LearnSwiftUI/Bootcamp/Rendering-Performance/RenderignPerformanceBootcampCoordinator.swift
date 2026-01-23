fef960d27fec2a36f4fd1a5e76278b35ad383990
HaoNguyen <>
2026-01-23 21:35:10 +0700

    Create Rendering Performance Bootcamp

---
+++ LearnSwiftUI/LearnSwiftUI/Bootcamp/Rendering-Performance/RenderignPerformanceBootcampCoordinator.swift
@@ -0,0 +1,42 @@
+//
+//  RenderignPerformanceBootcampCoordinator.swift
+//  LearnSwiftUI
+//
+//  Created by Hao Nguyen on 23/1/26.
+//
+
+import SwiftUI
+
+extension Router {
+    enum RenderignPerformanceRouter: String, Routable {
+        case exampleView
+        var id: String { self.rawValue }
+    }
+}
+
+struct RenderignPerformanceBootcampCoordinator: View, ScreenCoordinator {
+    typealias ScreenRouter = Router.RenderignPerformanceRouter
+    var navRouter: any NavRouterProtocol
+    
+    init(navRouter: any NavRouterProtocol) {
+        self.navRouter = navRouter
+    }
+    
+    var body: some View {
+        getView()
+    }
+    
+    @ViewBuilder
+    private func getView() -> some View {
+        RenderingPerformanceBootcamp()
+            .navigationTitle("Rendering & Performance")
+    }
+}
+
+extension RenderignPerformanceBootcampCoordinator {
+    func viewForRouter(router: Router.RenderignPerformanceRouter) -> some View {
+        switch router {
+        case .exampleView: EmptyView()
+        }
+    }
+}
---
+++ LearnSwiftUI/LearnSwiftUI/Bootcamp/Rendering-Performance/RenderingPerformanceBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  RenderingPerformanceBootcamp.swift
+//  LearnSwiftUI
+//
+//  Created by Hao Nguyen on 23/1/26.
+//
+
+import SwiftUI
+
+struct RenderingPerformanceBootcamp: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 24) {
+                ForEach(RenderingPerformanceLesson.all) { lesson in
+                    CodePreviewContainer(title: lesson.title, code: lesson.code, resultView: lesson.result?())
+                }
+            }
+        }
+    }
+}
