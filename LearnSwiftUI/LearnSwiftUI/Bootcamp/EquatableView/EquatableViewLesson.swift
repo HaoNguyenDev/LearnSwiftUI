2ab7aad1198285754841b38c05220f6bbd9f40dc
HaoNguyen <>
2026-01-25 23:12:19 +0700

    Create EquatableView bootcamp

---
+++ LearnSwiftUI/LearnSwiftUI/Bootcamp/EquatableView/EquatableViewLesson.swift
@@ -0,0 +1,16 @@
+//
+//  EquatableViewLesson.swift
+//  LearnSwiftUI
+//
+//  Created by Hao Nguyen on 25/1/26.
+//
+
+import Foundation
+
+struct EquatableViewLesson {
+    static let all = [
+        Lesson(title: "Lesson 1", code: """
+abcde
+""", result: nil)
+    ]
+}
