ec14069e88c976aee4d80e8dcbc449ad3d89c1dd
HaoNguyen <>
2026-01-28 13:02:20 +0700

    Update NavStack and NavPath explanation

--- LearnSwiftUI/LearnSwiftUI/Bootcamp/Navigation/NavigationLesson.swift
+++ LearnSwiftUI/LearnSwiftUI/Bootcamp/Navigation/NavigationLesson.swift
@@ -25,37 +25,82 @@ NavigationStack {
 
 """, result: nil), // NavigationStackExample()
                       
-                      Lesson(title: "Manage navigation paths", code: """
+                      Lesson(title: "NavigationPath — The Heart of Navigation", code: """
+What is NavigationPath?
+
+    @State private var path = NavigationPath()
+
+NavigationPath = a state array that describes the stack
+Each element = 1 screen
 NavigationStack allows you to manage the navigation stack programmatically via paths:
 
-struct ContentView: View {
+struct NavigationPathExample: View {
     @State private var path = NavigationPath()
     
     var body: some View {
         NavigationStack(path: $path) {
             VStack {
-                Button("Go to page 1") {
-                    path.append("Page1")
+                Button("Goto Page 1") {
+                    path.append("page1")
                 }
-                Button("Go to page 2") {
-                    path.append("Page2")
+                
+                Button("Goto Page 2") {
+                    path.append("page2")
                 }
             }
             .navigationDestination(for: String.self) { value in
-                if value == "Page1" {
-                    Page1View(path: $path)
-                } else if value == "Page2" {
-                    Page2View()
+                switch value {
+                case "page1":
+                    Page1Example(path: $path)
+                case "page2":
+                    Page2Example(path: $path)
+                default:
+                    EmptyView()
                 }
             }
-            .navigationTitle("Home")
+            .navigationTitle("NavigationPathExample")
         }
     }
 }
 
 """, result: nil), // NavigationPathExample()
-                      Lesson(title: "", code: """
+                      Lesson(title: "How to push, pop, popToRoot", code: """
+Example:
 
+Push:
+    path.append(item)
+
+Pop:
+    path.removeLast()
+
+Pop to root:
+    path.removeLast(path.count)
+
+struct Page1Example: View {
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        VStack(spacing: 24.0) {
+            Button("push to Page 2") {
+                path.append("page2")
+            }
+            
+            Button("pop") {
+                path.removeLast()
+            }
+            
+            Button("pop to root") {
+                path.removeLast(path.count)
+            }
+        }
+        .navigationDestination(for: String.self) { value in
+            if value == "page2" {
+                Page2Example(path: $path)
+            }
+        }
+        .navigationTitle("Page 1")
+    }
+}
 """, result: nil),
                       Lesson(title: "", code: """
 
