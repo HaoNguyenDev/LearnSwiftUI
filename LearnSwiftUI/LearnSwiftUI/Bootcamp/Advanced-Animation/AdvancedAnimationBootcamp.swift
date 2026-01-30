02c6a8233f20fa020dc4a1980f32c29750af1383
HaoNguyen <>
2026-01-30 22:51:55 +0700

    Update demo section for advanced animation bootcamop

--- LearnSwiftUI/LearnSwiftUI/Bootcamp/Advanced-Animation/AdvancedAnimationBootcamp.swift
+++ LearnSwiftUI/LearnSwiftUI/Bootcamp/Advanced-Animation/AdvancedAnimationBootcamp.swift
@@ -7,6 +7,30 @@
 
 import SwiftUI
 
+enum AdvancedAnimationDemoType: String, CaseIterable, Identifiable, Hashable {
+    case matchedGeometryEffect
+    case timelineAnimation
+    case phaseAnimation
+    
+    var id: String { rawValue }
+    
+    var title: String {
+        switch self {
+        case .matchedGeometryEffect: "matchedGeometryEffect demo"
+        case .timelineAnimation: "timelineAnimation demo"
+        case .phaseAnimation: "phaseAnimation demo"
+        }
+    }
+    
+    var demo: [Route.AdvancedAnimationBootcampRoute] {
+        switch self {
+        case .matchedGeometryEffect: [Route.AdvancedAnimationBootcampRoute.basicExample, Route.AdvancedAnimationBootcampRoute.tabbar, Route.AdvancedAnimationBootcampRoute.cardExpansionExample]
+        case .timelineAnimation: [Route.AdvancedAnimationBootcampRoute.basicExample]
+        case .phaseAnimation: [Route.AdvancedAnimationBootcampRoute.basicExample]
+        }
+    }
+}
+
 struct AdvancedAnimationBootcamp: View {
     var gotoDemo: SingleResult<Route.AdvancedAnimationBootcampRoute>?
     var body: some View {
@@ -18,14 +42,16 @@ struct AdvancedAnimationBootcamp: View {
             }
             
             LazyVStack(spacing: 24.0) {
-                ForEach(Route.AdvancedAnimationBootcampRoute.allCases) { demo in
+                ForEach(AdvancedAnimationDemoType.allCases) { type in
                     Section {
-                        demoTitle(demo.title)
-                            .onTapGesture {
-                                gotoDemo?(demo)
-                            }
+                        demoSection(type: type)
                     } header: {
-                        Text(demo.sectionTitle)
+                        Text(type.title)
+                            .foregroundStyle(.white)
+                            .frame(maxWidth: .infinity, maxHeight: 50)
+                            .background(.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .padding(.horizontal)
                     }
                 }
                 
@@ -36,6 +62,18 @@ struct AdvancedAnimationBootcamp: View {
 
 extension AdvancedAnimationBootcamp {
 
+    @ViewBuilder
+    private func demoSection(type: AdvancedAnimationDemoType) -> some View {
+        LazyVStack {
+            ForEach(type.demo) { demo in
+                demoTitle(demo.title)
+                    .onTapGesture {
+                        gotoDemo?(demo)
+                    }
+            }
+        }
+    }
+    
     @ViewBuilder
     private func demoTitle(_ title: String) -> some View {
         Text(title)
